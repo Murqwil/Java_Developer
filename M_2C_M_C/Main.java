dee73e770685357f006ffddc31c3f7c27b0094ed
Timur Sabirzynov <sabirzynov.timur@yandex.ru>
2024-04-14 20:00:42 +0300

    Добавил пару пунктов в классе Main,ветка готова к слиянию

--- M_2C_M_C/Main.java
+++ M_2C_M_C/Main.java
@@ -1,7 +1,16 @@
 package M_2C_M_C;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner visitorOrder = new Scanner(System.in);
+        while (true) {
+            System.out.print("Добрый день! Что желаете?");
+            String answer = visitorOrder.nextLine();
+            System.out.print("-- Что у вас есть в наличии?");
+            System.out.print("-- Сколько стоит ... ?(позиция + цена)");
+            System.out.print("-- Сколько будет делаться? (кол-во времени на изготовления)");
+        }
     }
 }
