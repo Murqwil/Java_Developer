021b1e7ed137460567f4d55227a9e9430da17c66
Timur Sabirzynov <sabirzynov.timur@yandex.ru>
2024-04-14 17:37:36 +0300

    Создал класс NotificationService

---
+++ M_2C_M_C/NotificationService.java
@@ -0,0 +1,8 @@
+package M_2C_M_C;
+/*
+Модуль уведомлений (Notification Service): Отвечает за отправку уведомлений
+клиентам о статусе заказа, оплаты и доставки.
+*/
+public class NotificationService {
+
+}
