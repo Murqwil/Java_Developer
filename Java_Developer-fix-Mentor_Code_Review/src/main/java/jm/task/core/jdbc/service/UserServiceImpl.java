a8463da85e7c22dac61581896352830d1d078251
Timur Sabirzynov <sabirzianov.timur@yandex.ru>
2024-05-16 11:26:21 +0300

    20240516 Изменения в задании,удалил множество Connection

---
+++ Java_Developer-fix-Mentor_Code_Review/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    //!Исправление: заприватить поля + зависим всегда от интерфейсов, а не от реализаций
+    private final UserDao userDao = new UserDaoJDBCImpl();
+/*
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+ */
+    @Override
+    public void createUsersTable() throws SQLException {
+        userDao.createUsersTable();
+    }
+
+    @Override
+    public void dropUsersTable() throws SQLException {
+        userDao.dropUsersTable();
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        userDao.saveUser(name, lastName, age);
+    }
+
+    @Override
+    public void removeUserById(long id) throws SQLException {
+        userDao.removeUserById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() throws SQLException {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public void cleanUsersTable() throws SQLException {
+        userDao.cleanUsersTable();
+    }
+}
